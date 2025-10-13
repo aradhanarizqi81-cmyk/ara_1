026d133e43d4028c7bd2bea6697c1a7781f31ef5
aradhanarizqi81-cmyk <aradhanarizqi81@gmail.com>
2025-10-13 16:33:44 +0700

    commit1

---
+++ Pertemuan2/tugasp2.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Daftar Produk</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 700px;
+            text-align: left;
+            font-family: Arial, sans-serif;
+        }
+
+        th {
+            background-color: #F4B183;
+            /* Warna orange header */
+            text-align: center;
+            font-size: 18px;
+            font-weight: bold;
+            padding: 8px;
+            border: 1px solid black;
+        }
+
+        td {
+            border: 1px solid black;
+            padding: 8px;
+            vertical-align: top;
+        }
+
+        .label {
+            background-color: #00FF80;
+            /* Hijau untuk label kiri */
+            width: 150px;
+            font-weight: bold;
+        }
+
+        .content {
+            background-color: #FF66FF;
+            /* Ungu muda untuk isi */
+        }
+
+        img {
+            width: 150px;
+            display: block;
+            margin: auto;
+        }
+    </style>
+</head>
+
+<body>
+    <table>
+        <tr>
+            <th colspan="3">DAFTAR PRODUK</th>
+        </tr>
+        <tr>
+            <td rowspan="3">
+                <img src="pikachu.jpg" Canon EOS M10">
+            </td>
+            <td class="label">Nama Produk</td>
+            <td class="content">Canon EOS M10 Kit EF-M 15-45mm</td>
+        </tr>
+        <tr>
+            <td class="label">Harga</td>
+            <td class="content">Rp 4,899,000</td>
+        </tr>
+        <tr>
+            <td class="label">Fitur Produk</td>
+            <td class="content">
+                <ul>
+                    <li>Kamera mirrorless</li>
+                    <li>Efektifitas Piksel : 18 MP</li>
+                    <li>Tipe sensor : CMOS DIGIC 6</li>
+                    <li>Layar : 3.0 Inch</li>
+                    <li>Memiliki lampu flash dengan jarak jangkauan hingga 5 meter</li>
+                </ul>
+            </td>
+        </tr>
+    </table>
+</body>
+<table>
+    <tr>
+        <th colspan="3">DAFTAR PRODUK</th>
+    </tr>
+    <tr>
+        <td rowspan="3">
+            <img src="jokowi.jpg">
+        </td>
+        <td class="label">nama</td>
+        <td class="content">jokowi dodo</td>
+    </tr>
+    <tr>
+        <td class="label">jabatan</td>
+        <td class="content">presiden</td>
+    </tr>
+    <tr>
+        <td class="label">keunggulan</td>
+        <td class="content">
+            <ul>
+                <li>ws 100 persen</li>
+                <li>pemegang kendali cuaca</li>
+                <li>mulyono</li>
+                <li>cungkirng</li>
+                <li>pdi</li>
+
+</html
---
+++ pertemuan4/tugas1.php
@@ -0,0 +1,16 @@
+<php> </php>
+<?php
+$bil1 = 200;
+$bil2 = 40;
+$hasil = $bil1 + $bil2;
+echo "Hasil dari $bil1 + $bil2 = $hasil<br>";
+$hasil = $bil1 - $bil2;
+echo "Hasil dari $bil1 - $bil2 = $hasil<br>";
+$hasil = $bil1 * $bil2;
+echo "Hasil dari $bil1 * $bil2 = $hasil<br>";
+$hasil = $bil1 / $bil2;
+echo "Hasil dari $bil1 / $bil2 = $hasil<br>";
+$hasil = $bil1 % $bil2;
+echo "Hasil dari $bil1 % $bil2 = $hasil<br>";
+?>
+<?php
