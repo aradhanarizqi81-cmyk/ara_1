ee5181985b4770a6fb84ffb9ed40fce922140406
aradhanarizqi81-cmyk <aradhanarizqi81@gmail.com>
2025-11-17 15:29:59 +0700

    pertemuan9

---
+++ pertemuan9/contohdowhile.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+    <title>Penggunaan DO WHILE</title>
+</head>
+
+<body>
+    Menggunakan DO WHILE
+    <br>
+    <?php
+    $jumlah = 1000;
+    do {
+        echo $jumlah++;
+        echo "<br>";
+    } while ($jumlah <= 1)
+    ?>
+</body>
+
+</html>
